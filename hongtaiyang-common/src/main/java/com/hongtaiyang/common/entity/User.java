16d68b6cd1666b732d288df476014b36be25be98
tangtuo <m17620078872@163.com>
2020-04-25 11:19:50 +0800

    引入自动校验参数的注解以及异常处理

--- hongtaiyang-common/src/main/java/com/hongtaiyang/common/entity/User.java
+++ hongtaiyang-common/src/main/java/com/hongtaiyang/common/entity/User.java
@@ -5,6 +5,7 @@ import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 
@@ -23,6 +24,7 @@ public class User {
     private Integer id;
     @ApiModelProperty(value = "用户名")
     @NotBlank(message = "用户名不能为空")
+    @Length(max = 12, message = "用户名长度不能超过12")
     private String userName;
     @ApiModelProperty(value = "密码")
     @NotBlank(message = "密码不能为空")
